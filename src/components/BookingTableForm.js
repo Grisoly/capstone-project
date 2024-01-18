3ce78e15718eab9cfb94b60a5aecd6d6da656ae4
Grisoly <120173770+Grisoly@users.noreply.github.com>
2024-01-17 19:01:14 -0800

    Adds table booking state

--- src/components/BookingTableForm.js
+++ src/components/BookingTableForm.js
@@ -1,22 +1,43 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import "../components/BookingTableForm.css";
+
+const timeReducer = (state, action) => {
+	switch (action.type) {
+		case "SET_AVAILABLE_TIMES":
+			return action.payload;
+		default:
+			return state;
+	}
+};
+const initializeTimes = () => {
+	return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+};
 
 function BookingTableForm() {
 	const [resDate, setResDate] = useState("");
 	const [resTime, setResTime] = useState("");
 	const [guests, setGuests] = useState(1);
 	const [occasion, setOccasion] = useState("Anniversary");
-	const [availableTimes] = useState([
-		"17:00",
-		"18:00",
-		"19:00",
-		"20:00",
-		"21:00",
-		"22:00",
-	]);
+	const [availableTimes, dispatch] = useReducer(
+		timeReducer,
+		[],
+		initializeTimes
+	);
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		console.log(resDate, resTime, guests, occasion);
+		setGuests("");
+		setResDate("");
+		setGuests(1);
+		setOccasion("Anniversary");
+	};
+	const handleDateChange = (e) => {
+		const newlySelectedDate = e.target.value;
+		setResDate(newlySelectedDate);
+		dispatch({
+			type: "SET_AVAILABLE_TIMES",
+			payload: { date: newlySelectedDate, times: initializeTimes() },
+		});
 	};
 
 	return (
@@ -26,7 +47,7 @@ function BookingTableForm() {
 				<input
 					type="date"
 					id="res-date"
-					onChange={(e) => setResDate(e.target.value)}
+					onChange={handleDateChange}
 					value={resDate}
 				/>
 				<label htmlFor="res-time">Choose time</label>
--- src/pages/Reservations.js
+++ src/pages/Reservations.js
@@ -1,7 +1,34 @@
 import React from "react";
+import BookingTableForm from "../components/BookingTableForm";
 
 function Reservations() {
-	return <div>Reservations</div>;
+	const [availableTimes, setAvailableTimes] = useState([
+		"17:00",
+		"18:00",
+		"19:00",
+		"20:00",
+		"21:00",
+		"22:00",
+	]);
+	const updateTimes = (selectedDate) => {
+		const newAvailableTimes = [
+			"17:00",
+			"18:00",
+			"19:00",
+			"20:00",
+			"21:00",
+			"22:00",
+		];
+		setAvailableTimes(newAvailableTimes);
+	};
+	return (
+		<div>
+			<BookingTableForm
+				updateTimes={updateTimes}
+				availableTimes={availableTimes}
+			/>
+		</div>
+	);
 }
 
 export default Reservations;
